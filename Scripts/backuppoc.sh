1f6bf3f9cca6f2dc7b3cfb79566e04804d639e26
Ron Evans <bills@cancunron.com>
2024-12-20 18:16:03 -0500

    24-12-20_18_15_55_commit

--- Scripts/backuppoc.sh
+++ Scripts/backuppoc.sh
@@ -2,11 +2,10 @@
 . "$(dirname "$PWD")"'/scripts/utils.sh'
 prnCmd 'n'  && setColor 'title'  && echo 'Backup POC directory and subdirectories'
 source="$(dirname "$(dirname "$PWD")")"
-setColor 'info' && echo "$source"
 dest='/mnt/d/onedrive/backups'
 now=$(date +%y-%m-%d_%H_%M_%S)
 archive="$(basename "$source")"'_Backup_'"$now"'.tgz'
 echo 'Creating file '"$archive"
-tar czf "$dest"'/'"$archive" -C '/'"$source"'/'
+tar czf "$dest"'/'"$archive" -C / "$source"'/'
 # Remove all but the 100 most recent files
-ls -dt "$dest"'/*' | tail -n +101 | xargs -d '\n' rm 2>/dev/null
+ls -dt "$dest/*" | tail -n +101 | xargs -d '\n' rm 2>/dev/null
