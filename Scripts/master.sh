e0ce694d6c8cc345cc53ef0c03d8ec01ce76a7a3
Ron Evans <bills@cancunron.com>
2024-12-20 23:46:21 -0500

    24-12-20_23_46_16_commit

--- Scripts/master.sh
+++ Scripts/master.sh
@@ -5,7 +5,7 @@ pardir="$(dirname $PWD)"
 scrdir="$pardir"'/scripts'
 # Get the remaining data that matches the first 2 characters of the passed argument
 getData() {
-    retval=''
+    retval=""
     # "${scriptargs[@]}" denotes each passed argument line
     for i in "${scriptargs[@]}"; do
         # Check until the first 2 characters of each passed argument line matches the data passed to the getData function
@@ -14,30 +14,30 @@ getData() {
     return
 }
 #*** Process banner data ***
-getData 'bn' || [[ -n "$retval" ]] && set +x && "$scrdir"'/banner.sh' "$retval"
+getData 'bn'; [ -n "$retval" ] && set +x && "$scrdir"'/banner.sh' "$retval"
 #*** Process special instructions data ***
-getData 'si' || [[ -n "$retval" ]] && set +x && "$scrdir"'/instructions.sh' "$retval"
+getData 'si'; [ -n "$retval" ] && set +x && "$scrdir"'/instructions.sh' "$retval"
 #*** Skip docker cleanup if $retval has data ***
-getData 'nd' || [[ -z "$retval" ]] && set +x && "$scrdir"'/dockerscrub.sh'
+getData nd; [ -z "$retval" ] && set +x && "$scrdir"'/dockerscrub.sh'
 #*** Remove files and/or subdirectories ***
-getData 'rm' || [[ -n "$retval" ]] && set +x && "$scrdir"'/filescrub.sh' "$retval"
+getData 'rm'; [ -n "$retval" ] && set +x && "$scrdir"'/filescrub.sh' "$retval"
 #*** Process copy code from source data ***
-getData 'cp' || [[ -n "$retval" ]] && set +x && "$scrdir"'/dofiles.sh' cp "$retval"
+getData 'cp'; [ -n "$retval" ] && set +x && "$scrdir"'/dofiles.sh' cp "$retval"
 #*** Display Dockerfiles ***
-getData 'ds' || [[ -z "$retval" ]] && set +x && "$scrdir"'/dispfiles.sh' df
+getData 'ds'; [ -z "$retval" ] && set +x && "$scrdir"'/dispfiles.sh' df
 #*** Skip display script files if $retval has data ***
-getData 'ns' || [[ -z "$retval" ]] && set +x && "$scrdir"'/dispscripts.sh'
+getData 'ns'; [ -z "$retval" ] && set +x && "$scrdir"'/dispscripts.sh'
 #*** Display docker-compose.yml ***
-getData 'ds' || [[ -z "$retval" ]] && set +x && "$scrdir"'/dispfiles.sh' dc
+getData 'ds'; [ -z "$retval" ] && set +x && "$scrdir"'/dispfiles.sh' dc
 #*** Display configuration files ***
-getData 'ds' || [[ -z "$retval" ]] && set +x && "$scrdir"'/dispfiles.sh' cf
+getData 'ds'; [ -z "$retval" ] && set +x && "$scrdir"'/dispfiles.sh' cf
 #*** Process execute docker build and run data ***
-getData 'br' || [[ -n "$retval" ]] && set +x && "$scrdir"'/dofiles.sh' br "$retval"
+getData 'br'; [ -n "$retval" ] && set +x && "$scrdir"'/dofiles.sh' br "$retval"
 #*** Process show results data ***
-getData 'sr' || [[ -n "$retval" ]] && set +x && "$scrdir"'/dofiles.sh' sr "$retval"
+getData 'sr'; [ -n "$retval" ] && set +x && "$scrdir"'/dofiles.sh' sr "$retval"
 #*** Backup all POC files ***
 getData 'bu'
-if [[ -z "$retval" ]]; then
+if [ -z "$retval" ]; then
     set +x && "$scrdir"'/backupPOC.sh'
 elif [ "${retval:0}" != x ]; then
     set +x && nohup "$scrdir"'/backupPOC.sh'
