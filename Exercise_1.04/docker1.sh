639edc18db599203293ce65901ef0e625dea9a1e
Ron Evans <bills@cancunron.com>
2024-12-20 22:18:13 -0500

    24-12-20_22_18_02_commit

--- Exercise_1.04/docker1.sh
+++ Exercise_1.04/docker1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 pardir="$(dirname "$PWD")"
 # Banner:
-arg1="bnExercise 1.4.T1: Missing Dependencies"
+arg1='bnExercise 1.4.T1: Missing Dependencies'
 # Display special instructions:
-arg2="siWhen you see the message sh: #: curl: not found
+arg2='siWhen you see the message sh: #: curl: not found
 1. Open a 2nd Terminal and press ./docker2.sh
-2. When you see a block of html code in the 1st Terminal, press Ctrl-C"
+2. When you see a block of html code in the 1st Terminal, press Ctrl-C'
 # Show result commands:
-arg3="srdocker run -it --name lister ubuntu sh -c 'website=helsinki.fi; while true; do
-echo Searching for \$website ..; sleep 1; curl http://\$website; sleep 30; done'"
+arg3='srdocker run -it --name lister ubuntu sh -c "website=helsinki.fi; while true; do
+echo Searching for \$website ..; sleep 1; curl http://\$website; sleep 30; echo \$website; done"'
 # Execute it!
 $pardir/scripts/master.sh "$arg1" "$arg2" "$arg3"
--- Exercise_1.04/docker2.sh
+++ Exercise_1.04/docker2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 pardir="$(dirname "$PWD")"
 # Banner:
-arg1="bnExercise 1.4.T2: Missing Dependencies"
+arg1='bnExercise 1.4.T2: Missing Dependencies'
 # No dockerscrub:
-arg2="ndx"
+arg2='ndx'
 # Build and run commands:
-arg3="brdocker exec -it lister bash -c 'apt-get -y update; apt-get -y install curl'"
+arg3='brdocker exec -it lister bash -c "apt-get -y update; apt-get -y install curl"'
 # No backup
-arg4="bux"
+arg4='bux'
 # Execute it!
 $pardir/scripts/master.sh "$arg1" "$arg2" "$arg3" "$arg4"
