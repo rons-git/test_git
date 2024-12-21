1b3e615e4c506fbb8be6f52af11e9c977a3d44ae
Ron Evans <bills@cancunron.com>
2024-12-20 21:25:54 -0500

    24-12-20_21_25_49_commit

--- Exercise_1.04/docker1.sh
+++ Exercise_1.04/docker1.sh
@@ -7,7 +7,7 @@ arg2='siWhen you see the message sh: #: curl: not found
 1. Open a 2nd Terminal and press ./docker2.sh
 2. When you see a block of html code in the 1st Terminal, press Ctrl-C'
 # Show result commands:
-arg3='srdocker run -it --name lister ubuntu sh -c 
-"website=helsinki.fi; while true; do echo Searching for \$website ..; sleep 5; curl http://\$website; sleep 30; done"'
+arg3="srdocker run -it --name lister ubuntu sh -c website=helsinki.fi; while true;
+do echo Searching for \$website ..; sleep 5; curl http://\$website; sleep 30; done"
 # Execute it!
 $pardir/scripts/master.sh "$arg1" "$arg2" "$arg3"
